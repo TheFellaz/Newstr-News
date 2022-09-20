5045ba13b60957c21785d0b5ad8ca85271613ac5
zjp292 <72166103+zjp292@users.noreply.github.com>
2022-09-20 15:47:08 -0700

    Add files via upload
    This is the spring framework for the web app.

---
+++ Newstr/src/main/java/com/ZJJWH/Newstr/NewstrApplication.java
@@ -0,0 +1,13 @@
+package com.ZJJWH.Newstr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewstrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewstrApplication.class, args);
+	}
+
+}
---
+++ Newstr/src/test/java/com/ZJJWH/Newstr/NewstrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ZJJWH.Newstr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewstrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
