623d1c8d09d5f0fccf367032b41ba490b8268f62
zjp292 <72166103+zjp292@users.noreply.github.com>
2022-09-23 13:10:56 -0700

    Add files via upload
    added working file

---
+++ Newster-news/src/test/java/com/example/Newsternews/NewsterNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Newsternews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsterNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
