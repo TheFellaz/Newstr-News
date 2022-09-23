00c69ba33c6a2662fbc88e5fa19f7e2b5b6b1daa
zjp292 <72166103+zjp292@users.noreply.github.com>
2022-09-23 09:49:51 -0700

    Add files via upload

---
+++ Newster-news/src/main/java/com/example/Newsternews/NewsterNewsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Newsternews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsterNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsterNewsApplication.class, args);
+	}
+
+}
