ea185f75bf396164110caa2018fe47219c7bd276
Hyunsoo Kim <hyunsoo99kim@gmail.com>
2022-11-03 15:56:40 -0700

    Update Router.js #63

--- newsterweb/src/Router.js
+++ newsterweb/src/Router.js
@@ -3,12 +3,19 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUp from "./component/SignUpPage/SignUp";
 import NotFound from "./component/NotFound/NotFound";
+import LoginPage from "./component/LoginPage/LoginPage";
+import MainPage from "./component/MainPage/MainPage";
+import UserProfilePage from "./component/UserProfilePage/UserProfilePage";
 
 function Router(props) {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SignUp />} />
+        <Route path="/" element={<MainPage />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/user" element={<UserProfilePage />} />
+        <Route path="/user/:id" element={<UserProfilePage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
