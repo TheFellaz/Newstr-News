31272c13221105fc8aad58625decf4d411d226a5
zjp292 <zjp29@nau.edu>
2022-11-11 18:41:53 -0700

    Re-added a new API key. Fixes #74

--- Newster-news/src/main/java/com/example/Newsternews/Keys/Keys.java
+++ Newster-news/src/main/java/com/example/Newsternews/Keys/Keys.java
@@ -6,7 +6,7 @@ public class Keys {
      * news API service
      **/
 
-    public static String APIKEY = "b82606bfe6904e199c3197562ca42b6a";
+    public static String APIKEY = "eb6c666ab12449e99a5f7349c4c4b2a2";
 
     public static String ENDPOINT = "https://api.bing.microsoft.com/";
     public static String PATH = "/v7.0/news/search";
