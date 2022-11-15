fd0f20b91de5ebc61e5c09cbd8275561222c88b7
Jacob Olson <jdo227@nau.edu>
2022-11-15 14:28:47 -0700

    Update SignUp.js

--- newsterweb/src/component/SignUpPage/SignUp.js
+++ newsterweb/src/component/SignUpPage/SignUp.js
@@ -8,13 +8,6 @@ function SignUp() {
       <h3>Sign Up page</h3>
       <input placeholder="your email"></input>
       <input placeholder="Password"></input>
-      <select name="types" id="news_types">
-        <option value="None">None</option>
-        <option value="Sports">Sports</option>
-        <option value="Politics">Politics</option>
-        <option value="Economics">Economics</option>
-        <option value="Business">Business</option>
-      </select>
 
       <button
         className="submitButton"
