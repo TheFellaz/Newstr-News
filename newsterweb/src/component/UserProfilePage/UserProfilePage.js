2c43edd316be48bc4b62e12ab3036aebc607d19f
Jacob Olson <jdo227@nau.edu>
2022-11-17 16:53:27 -0700

    Update UserProfilePage.js and create TopicList utility

--- newsterweb/src/component/UserProfilePage/UserProfilePage.js
+++ newsterweb/src/component/UserProfilePage/UserProfilePage.js
@@ -1,8 +1,74 @@
 import React from "react";
+import { TOPICS } from "../../utils/TopicList";
 import "./UserProfilePage.css";
 
 function UserProfilePage(props) {
-  return <div>User Profile Page</div>;
+  let username = "me, baby";
+  let userTopicsList = [0, 1, 2, 3, 4];
+  let frequencySelection =  1;
+  let email = "ayyyyyye lmao";
+  return (
+    <div>
+      <div id="topLeftUserInfoID">
+        Welcome back, {username}. <br />
+        Your preferred email for news is currently {email}.
+      </div>
+      <div id="topicListID">
+        {TOPICS.map((topicName, topicIndex) =>
+          GenerateTopicCheckbox(topicName, topicIndex, userTopicsList)
+        )}
+      </div>
+    </div>
+  );
+}
+
+function GenerateTopicCheckbox(topicName, topicIndex, userTopicList) {
+  {
+    if (topicIndex in userTopicList) {
+      return (
+        <div>
+          <input
+            id="{topicName}ID"
+            type="checkbox"
+            name="{topicName}"
+            value="true"
+            defaultChecked
+          ></input>
+          <label for="topicName}ID">{topicName}</label>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <input
+            id="{topicName}ID"
+            type="checkbox"
+            name="{topicName}"
+            value="true"
+          ></input>
+          <label for="topicName}ID">{topicName}</label>
+        </div>
+      );
+    }
+  }
+}
+
+function GenerateFrequencyCheckboxes(userFrequencySelection) {
+  let frequencyOptions = ["Morning", "Morning and Evening", "Morning, Noon, and Evening"];
+  let returnString = "";
+
+  for(index = 0, index < 3, index++) {
+    if((index + 1) == userFrequencySelection) {
+      returnString += '<input type="checkbox" id=' + 
+      frequencyOptions[index] + 
+      ' name="frequency" value="' +
+      (index + 1) +
+      '" defaultChecked></input>\n
+      <label for="'
+    })
+
+  }
+
 }
 
 export default UserProfilePage;
