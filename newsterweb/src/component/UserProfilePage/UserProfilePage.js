cd2efcbe7399d8288f798bc4ff0f2a06fd487781
Jacob Olson <jdo227@nau.edu>
2022-11-17 19:17:07 -0700

    Update UserProfilePage.js

--- newsterweb/src/component/UserProfilePage/UserProfilePage.js
+++ newsterweb/src/component/UserProfilePage/UserProfilePage.js
@@ -3,21 +3,38 @@ import { TOPICS } from "../../utils/TopicList";
 import "./UserProfilePage.css";
 
 function UserProfilePage(props) {
-  let username = "me, baby";
+  let username = "sexy";
   let userTopicsList = [0, 1, 2, 3, 4];
-  let frequencySelection =  1;
+  let frequencySelection = 1;
   let email = "ayyyyyye lmao";
+  const frequencyOptions = [
+    "Morning",
+    "Morning and Evening",
+    "Morning, Noon, and Evening",
+  ];
   return (
     <div>
       <div id="topLeftUserInfoID">
         Welcome back, {username}. <br />
         Your preferred email for news is currently {email}.
       </div>
+
       <div id="topicListID">
         {TOPICS.map((topicName, topicIndex) =>
           GenerateTopicCheckbox(topicName, topicIndex, userTopicsList)
         )}
       </div>
+
+      <fieldset id="frequencyOptionsID">
+        <legend>Select an Email Frequency</legend>
+        {frequencyOptions.map((frequencyOption, frequencyIndex) =>
+          GenerateFrequencyRadio(
+            frequencySelection,
+            frequencyOption,
+            frequencyIndex
+          )
+        )}
+      </fieldset>
     </div>
   );
 }
@@ -28,47 +45,62 @@ function GenerateTopicCheckbox(topicName, topicIndex, userTopicList) {
       return (
         <div>
           <input
-            id="{topicName}ID"
+            id={topicName}
             type="checkbox"
-            name="{topicName}"
+            name={topicName}
             value="true"
             defaultChecked
           ></input>
-          <label for="topicName}ID">{topicName}</label>
+          <label for={topicName}>{topicName}</label>
         </div>
       );
     } else {
       return (
         <div>
           <input
-            id="{topicName}ID"
+            id={topicName}
             type="checkbox"
-            name="{topicName}"
+            name={topicName}
             value="true"
           ></input>
-          <label for="topicName}ID">{topicName}</label>
+          <label for={topicName}>{topicName}</label>
         </div>
       );
     }
   }
 }
 
-function GenerateFrequencyCheckboxes(userFrequencySelection) {
-  let frequencyOptions = ["Morning", "Morning and Evening", "Morning, Noon, and Evening"];
-  let returnString = "";
-
-  for(index = 0, index < 3, index++) {
-    if((index + 1) == userFrequencySelection) {
-      returnString += '<input type="checkbox" id=' + 
-      frequencyOptions[index] + 
-      ' name="frequency" value="' +
-      (index + 1) +
-      '" defaultChecked></input>\n
-      <label for="'
-    })
-
+function GenerateFrequencyRadio(
+  userFrequencySelection,
+  frequencyOptionName,
+  frequencyOptionIndex
+) {
+  if (frequencyOptionIndex == userFrequencySelection) {
+    return (
+      <div className="frequencyOption">
+        <input
+          type="radio"
+          id={frequencyOptionName}
+          name="frequency"
+          value={frequencyOptionIndex + 1}
+          defaultChecked
+        ></input>
+        <label for={frequencyOptionName}>{frequencyOptionName}</label>
+      </div>
+    );
+  } else {
+    return (
+      <div className="frequencyOption">
+        <input
+          type="radio"
+          id={frequencyOptionName}
+          name="frequency"
+          value={frequencyOptionIndex + 1}
+        ></input>
+        <label for={frequencyOptionName}>{frequencyOptionName}</label>
+      </div>
+    );
   }
-
 }
 
 export default UserProfilePage;
