300ed58909b9c115b23092d95b440d64646b96ef
Zachary Parham <zjp29@nau.edu>
2022-11-18 18:15:50 -0700

    work on database from monday, Fixes #83

---
+++ Newster-news/src/main/java/com/example/Newsternews/Resources/DBUserData.java
@@ -0,0 +1,46 @@
+package com.example.Newsternews.Resources;
+
+
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Entity
+//@Table(name = "user")
+public class DBUserData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
+}
