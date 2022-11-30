5df1faf3af50efb6811843e41d2943d5769415a5
William Rogers <williamrogers2002@gmail.com>
2022-11-30 15:06:48 -0700

    Added query assertion test

--- Newster-news/src/test/java/com/example/Newsternews/APITests/API_Tests.java
+++ Newster-news/src/test/java/com/example/Newsternews/APITests/API_Tests.java
@@ -3,10 +3,16 @@ package com.example.Newsternews.APITests;
 
 import com.example.Newsternews.API.API;
 import com.example.Newsternews.API.ArticleClass;
+import com.example.Newsternews.Keys.Keys;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @RunWith(JUnit4.class)
@@ -22,9 +28,23 @@ public class API_Tests {
         String name = result.getHead().getName();
         String description = result.getHead().getDescription();
 
+        //Assert that the API returns a url, name, and description
         assertNotNull(url);
         assertNotNull(name);
         assertNotNull(description);
+
+    }
+
+    @Test
+    public void assertCorrectQuery() throws Exception
+    {
+        String testString = API.SearchNews();
+
+        JsonElement jelement = new JsonParser().parse(testString);
+        JsonObject jobject = jelement.getAsJsonObject();
+        String testQuery = jobject.get("queryContext").getAsJsonObject().get("originalQuery").getAsString();
+
+        assertEquals(testQuery, Keys.SEARCHTERM);
 
     }
 
