79096637bbafbfa15b9a49eedede3634bab3a725
William Rogers <williamrogers2002@gmail.com>
2022-11-30 15:24:04 -0700

    Added web integration tests template

---
+++ Newster-news/src/test/java/com/example/Newsternews/WebIntegrationTests/WebIntegration_Tests.java
@@ -0,0 +1,15 @@
+package com.example.Newsternews.WebIntegrationTests;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class WebIntegration_Tests
+{
+    @Test
+    public void testMethods() throws Exception
+    {
+
+    }
+}
