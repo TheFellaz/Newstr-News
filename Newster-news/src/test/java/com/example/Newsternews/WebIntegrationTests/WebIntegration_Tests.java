27b25370c557c91b146bde5849c9485b9e4b86e0
William Rogers <williamrogers2002@gmail.com>
2022-11-30 15:31:12 -0700

    Added token obfuscation test

--- Newster-news/src/test/java/com/example/Newsternews/WebIntegrationTests/WebIntegration_Tests.java
+++ Newster-news/src/test/java/com/example/Newsternews/WebIntegrationTests/WebIntegration_Tests.java
@@ -3,13 +3,20 @@ package com.example.Newsternews.WebIntegrationTests;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import com.example.Newsternews.Resources.token;
 
-@RunWith(JUnit4.class)
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 public class WebIntegration_Tests
 {
     @Test
-    public void testMethods() throws Exception
+    public void tokenObfuscation() throws Exception
     {
+        String testID = "testforobfuscation";
 
+        String obfuscatedID = token.createToken(testID);
+
+        //Basic assertion that ID is obfuscated
+        assertNotEquals(obfuscatedID, testID);
     }
 }
