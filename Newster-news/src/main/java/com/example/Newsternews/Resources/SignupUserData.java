2bd716437c09de522c61dd967e8df305a31a57b4
zjp292 <zjp29@nau.edu>
2022-11-29 20:32:44 -0700

    user data file

---
+++ Newster-news/src/main/java/com/example/Newsternews/Resources/SignupUserData.java
@@ -0,0 +1,25 @@
+package com.example.Newsternews.Resources;
+
+
+public class SignupUserData {
+
+    private String username;
+    private String email;
+    private String password;
+
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
+}
