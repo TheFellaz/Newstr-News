9e8e0702483282b85bce25e0b5115cdbfa182f9a
Hyunsoo Kim <hyunsoo99kim@gmail.com>
2022-12-03 14:44:40 -0700

    Add userRepositoryTest for #100

---
+++ Newster-news/src/test/java/com/example/Newsternews/DB_CRUD_Tests/userRepositoryTest.java
@@ -0,0 +1,66 @@
+package com.example.Newsternews.DB_CRUD_Tests;
+
+
+import com.example.Newsternews.Resources.User;
+import com.example.Newsternews.userRepository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class userRepositoryTest {
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    public void testInsertUser(){
+        IntStream.rangeClosed(1,10).forEach(i -> {
+
+            String name = "name"+i;
+            String email = "email"+i;
+            String pw = "pw"+i;
+            User user = new User(name, email, pw);
+            // 저장한다
+            userRepository.save(user);
+        });
+        System.out.println(userRepository.getClass().getName());
+        IntStream.rangeClosed(5,10).forEach(i -> {
+
+            String name = "name2"+i;
+            String email = "email"+i;
+            String pw = "pw2"+i;
+            User user = new User(name, email, pw);
+            // 저장한다
+            userRepository.save(user);
+        });
+        System.out.println(userRepository.getClass().getName());
+    }
+
+    @Test
+    public void testSelect(){
+        Long mno = 100L;
+//        Optional<User> result = userRepository.findById(mno);
+//        System.out.println("================");
+//        if(result.isPresent()){
+//            User user = result.get();
+//            System.out.println(user);
+//        }
+        User tmpUser = userRepository.findByEmail("email1").get(0);
+//        System.out.println(userResult.toString());
+//        User tmpUser = userRepository.findByEmail("email1");
+        System.out.println(tmpUser.toString());
+    }
+
+    @Test
+    public void testUpdate(){
+        User user = new User("Kim", "hk486@nau.edu", "pw", "[1,2,3]", 3, "token");
+        System.out.println("1-------------");
+        userRepository.save(user);
+        System.out.println("2-------------");
+    }
+
+}
