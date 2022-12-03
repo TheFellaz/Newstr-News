154fbe9c4b9497e4972238292d8c3cd7f7beef8e
Hyunsoo Kim <hyunsoo99kim@gmail.com>
2022-12-03 14:43:48 -0700

    Add userRepository for DB #99

---
+++ Newster-news/src/main/java/com/example/Newsternews/userRepository/UserRepository.java
@@ -0,0 +1,21 @@
+package com.example.Newsternews.userRepository;
+
+
+import com.example.Newsternews.Resources.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query(value = "SELECT * FROM user_tb", nativeQuery = true)
+    List<User> findAll();
+
+    @Query(value = "SELECT * FROM user_tb WHERE user_email = ?1", nativeQuery = true)
+    List<User> findByEmail(String email);
+
+//    @Query(value = "")
+}
