05c816a6dabd3ca9234e1f0590d0a4df571b0aac
Hyunsoo Kim <hyunsoo99kim@gmail.com>
2022-12-03 14:42:22 -0700

    Update NewsterNewApplicaton for User Shcema #96

--- Newster-news/src/main/java/com/example/Newsternews/NewsterNewsApplication.java
+++ Newster-news/src/main/java/com/example/Newsternews/NewsterNewsApplication.java
@@ -2,10 +2,14 @@ package com.example.Newsternews;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
+@EntityScan("com.example")
+@EnableJpaRepositories("com.example")
 public class NewsterNewsApplication {
 
 	public static void main(String[] args) {
