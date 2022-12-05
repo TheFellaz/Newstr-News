6a59622f98bd289c42393f2e4f04beb61dbe9915
Jacob Olson <jdo227@nau.edu>
2022-12-04 17:07:24 -0700

    Update Jest and babel config files

--- newsterweb/src/babel.config.js
+++ newsterweb/src/babel.config.js
@@ -1,6 +1,11 @@
 module.exports = {
-    presets: [
-      '@babel/preset-env',
-      ['@babel/preset-react', {runtime: 'automatic'}],
-    ],
-  };
+  presets: [
+    ["@babel/preset-env", { targets: { node: "current" } }],
+    [("@babel/preset-react", { runtime: "automatic" })],
+  ],
+  env: {
+    test: {
+      plugins: ["@babel/plugin-transform-modules-commonjs"],
+    },
+  },
+};
